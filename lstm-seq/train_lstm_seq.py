3cccc0989e448d91e02a38ff1fe7e10d98ff34b2
Gabbi Fisher <gabbifisher@gmail.com>
2017-03-16 17:27:12 -0700

    first attempt at fleshed out code for tsne for doc2vec

--- lstm-seq/train_lstm_seq.py
+++ lstm-seq/train_lstm_seq.py
@@ -3,6 +3,9 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 
+print('Loading data...')
+
+print('Build model...')
 in_out_neurons = 2
 hidden_neurons = 300
 
@@ -10,4 +13,8 @@ model = Sequential()
 model.add(LSTM(in_out_neurons, hidden_neurons, return_sequences=False))
 model.add(Dense(hidden_neurons, in_out_neurons))
 model.add(Activation("linear"))
-model.compile(loss="mean_squared_error", optimizer="rmsprop")
+model.compile(loss="mean_squared_error", optimizer="adam")
+
+print('Train...')
+model.fit(x_train, y_train, batch_size=batch_size, epochs=15,
+          validation_data=(x_test, y_test)) # do not want validation_data! will run clustering tests!
