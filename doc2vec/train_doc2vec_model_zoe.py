537c89fd98168a86da087dec2c95934fe0c3f753
Zoe Robert <zrobert7@stanford.edu>
2017-03-17 13:37:46 -0700

    ddd

--- doc2vec/train_doc2vec_model_zoe.py
+++ doc2vec/train_doc2vec_model_zoe.py
@@ -156,6 +156,7 @@ def get_accuracy(model):
             article_1_vec = model.infer_vector(clean_test_articles[article_1_index])
         except Exception, e:
             print "article 1 fucked up!"
+            print e
             print clean_test_articles[article_1_index]
 
         try:
